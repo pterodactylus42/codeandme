22a527f56969ba1ea72b007a8b5da4543d7204d7
pterodactylus42 <pterodactylus43@gmail.com>
2025-03-02 15:52:48 +0100

    add visitors && console

--- custom_builder/com.codeandme.custombuilder/src/com/codeandme/custombuilder/builders/MyBuilder.java
+++ custom_builder/com.codeandme.custombuilder/src/com/codeandme/custombuilder/builders/MyBuilder.java
@@ -10,12 +10,28 @@
  *******************************************************************************/
 package com.codeandme.custombuilder.builders;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 public class MyBuilder extends IncrementalProjectBuilder {
 
@@ -25,9 +41,8 @@ public class MyBuilder extends IncrementalProjectBuilder {
 	protected IProject[] build(final int kind, final Map<String, String> args, final IProgressMonitor monitor)
 			throws CoreException {
 
-		System.out.println("Custom builder triggered");
+		printToConsole("MyBuilder.build triggered");
 
-		// get the project to build
 		getProject();
 
 		switch (kind) {
@@ -44,4 +59,90 @@ public class MyBuilder extends IncrementalProjectBuilder {
 
 		return null;
 	}
+	
+	class MyDeltaVisitor implements IResourceDeltaVisitor {
+		@Override
+		public boolean visit(IResourceDelta delta) throws CoreException {
+			printToConsole("MyDeltaVisitor.visit triggered");
+			IResource resource = delta.getResource();
+			switch (delta.getKind()) {
+			case IResourceDelta.ADDED:
+				process(resource);
+				break;
+			case IResourceDelta.REMOVED:
+				//
+				break;
+			case IResourceDelta.CHANGED:
+				process(resource);
+				break;
+			}
+			// return true to continue visiting children.
+			return true;
+		}
+	}
+
+	class MyResourceVisitor implements IResourceVisitor {
+		@Override
+		public boolean visit(IResource resource) {
+			printToConsole("MyResourceVisitor.visit triggered");
+			process(resource);
+			// return true to continue visiting children.
+			return true;
+		}
+	}
+	
+	private void process(IResource resource) {
+		printToConsole("process resource" + resource.getName());
+		
+	}
+	
+	private void printToConsole(final String message)
+	{
+		final MessageConsole console = getConsole();
+		final MessageConsoleStream messageStream = console.newMessageStream();
+
+		messageStream.println(message);
+		show(console);
+		
+		try {
+			messageStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private MessageConsole getConsole()
+	{
+		final String name = "MyBuilder";
+
+		  ConsolePlugin plugin = ConsolePlugin.getDefault();
+		  IConsoleManager conMan = plugin.getConsoleManager();
+		  IConsole[] existing = conMan.getConsoles();
+		  for (int i = 0; i < existing.length; i++)
+		     if (name.equals(existing[i].getName()))
+		        return (MessageConsole) existing[i];
+
+		  MessageConsole myConsole = new MessageConsole(name, null);
+		  conMan.addConsoles(new IConsole[]{myConsole});
+		  return myConsole;
+	}
+
+	private void show(final MessageConsole console) {
+	  Display.getDefault().asyncExec(new Runnable() {
+		    @Override
+		    public void run() {
+		        try {
+					IWorkbenchWindow win = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+					IWorkbenchPage page = win.getActivePage();
+					String id = IConsoleConstants.ID_CONSOLE_VIEW;
+					IConsoleView view = (IConsoleView) page.showView(id);
+					  view.display(console);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+		    }
+		});
+	}
+
+
 }
